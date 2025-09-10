2b4ba915a697fb7b3d6e424e3ef4e3af2ad9fed2
Patrick M. Nash <31717677+PatrickMNash@users.noreply.github.com>
2025-09-10 15:34:22 -0400

    dreams of joy

---
+++ index.js
@@ -0,0 +1,21 @@
+function loadArticle (articleName) {
+  fetch(articleName)
+    .then(response => {
+      if (response.ok) {
+        return response.text();
+      }
+      throw new Error('Network response was not ok.');
+    })
+    .then(html => {
+      document
+        .getElementById("main")
+        .innerHTML = html;
+    })
+    .catch(error => console.error('There was a problem with the fetch operation:', error));
+}
+
+function main () {
+  loadArticle("./dreams-of-joy.html");
+}
+
+window.onload = () => main();
