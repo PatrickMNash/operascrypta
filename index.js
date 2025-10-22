fd285f4510aa2ecd8fd1e4a52721aed4429b3e71
Patrick M. Nash <31717677+PatrickMNash@users.noreply.github.com>
2025-10-22 15:19:07 -0400

    the self-inflicted crisis

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function loadArticle (articleName) {
 }
 
 function main () {
-  loadArticle("./sunbathing-in-a-cemetery.html");
+  loadArticle("./the-self-inflicted-crisis.html");
 }
 
 window.onload = () => main();
