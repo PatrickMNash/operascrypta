9c68858bb6ea443537a3f83a0b1957e13b32a626
Patrick M. Nash <31717677+PatrickMNash@users.noreply.github.com>
2025-11-05 13:02:11 -0500

    the zombie-virus squirrel

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function loadArticle (articleName) {
 }
 
 function main () {
-  loadArticle("./the-self-inflicted-crisis.html");
+  loadArticle("./zombie-virus-squirrel.html");
 }
 
 window.onload = () => main();
